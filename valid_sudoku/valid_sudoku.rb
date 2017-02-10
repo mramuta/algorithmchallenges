7c0b3919fd11cd32d631a2acba782829068a337e
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-10 15:58:25 -0600

    make ruby solution much faster

--- valid_sudoku/valid_sudoku.rb
+++ valid_sudoku/valid_sudoku.rb
@@ -2,58 +2,78 @@
 # @return {Boolean}
 def is_valid_sudoku(board)
   board = board.map{|row| row.split('')}
-  return false if !valid_arr(board[0][0..2] + board[1][0..2] + board[2][0..2])
-  return false if !valid_arr(board[0][3..5] + board[1][3..5] + board[2][3..5])
-  return false if !valid_arr(board[0][6..8] + board[1][6..8] + board[2][6..8])
-  return false if !valid_arr(board[3][0..2] + board[4][0..2] + board[5][0..2])
-  return false if !valid_arr(board[3][3..5] + board[4][3..5] + board[5][3..5])
-  return false if !valid_arr(board[3][6..8] + board[4][6..8] + board[5][6..8])
-  return false if !valid_arr(board[6][0..2] + board[7][0..2] + board[8][0..2])
-  return false if !valid_arr(board[6][3..5] + board[7][3..5] + board[8][3..5])
-  return false if !valid_arr(board[6][6..8] + board[7][6..8] + board[8][6..8])
-  i = 0
-  while i < 9
-    return false if !valid_arr(board_col(board,i))
-    i += 1
-  end
-  i = 0
-  while i < 9
-    return false if !valid_arr(board_row(board,i))
-    i += 1
-  end
+  return false if contains_duplicate(board[0])
+  return false if contains_duplicate(board[1])
+  return false if contains_duplicate(board[2])
+  return false if contains_duplicate(board[3])
+  return false if contains_duplicate(board[4])
+  return false if contains_duplicate(board[5])
+  return false if contains_duplicate(board[6])
+  return false if contains_duplicate(board[7])
+  return false if contains_duplicate(board[8])
+  return false if contains_duplicate(board[0][0..2] + board[1][0..2] + board[2][0..2])
+  return false if contains_duplicate(board[0][3..5] + board[1][3..5] + board[2][3..5])
+  return false if contains_duplicate(board[0][6..8] + board[1][6..8] + board[2][6..8])
+  return false if contains_duplicate(board[3][0..2] + board[4][0..2] + board[5][0..2])
+  return false if contains_duplicate(board[3][3..5] + board[4][3..5] + board[5][3..5])
+  return false if contains_duplicate(board[3][6..8] + board[4][6..8] + board[5][6..8])
+  return false if contains_duplicate(board[6][0..2] + board[7][0..2] + board[8][0..2])
+  return false if contains_duplicate(board[6][3..5] + board[7][3..5] + board[8][3..5])
+  return false if contains_duplicate(board[6][6..8] + board[7][6..8] + board[8][6..8])
+  board = board.transpose
+  return false if contains_duplicate(board[0])
+  return false if contains_duplicate(board[1])
+  return false if contains_duplicate(board[2])
+  return false if contains_duplicate(board[3])
+  return false if contains_duplicate(board[4])
+  return false if contains_duplicate(board[5])
+  return false if contains_duplicate(board[6])
+  return false if contains_duplicate(board[7])
+  return false if contains_duplicate(board[8])
   true
 end
 
 def contains_duplicate(arr)
-  nums = {}
-  arr.each do |element|
-    if nums[element] && element != '.'
-      return true
-    end
-    nums[element] = true
-  end
-  false
+  arr = arr - ['.']
+  arr.uniq.length != arr.length
 end
 
-def valid_arr(arr)
-  if contains_duplicate(arr) || !(arr - ['1','2','3','4','5','6','7','8','9','.']).empty?
-    return false
-  end
-  true
-end
 
-def board_col(puzzle, row_index)
-  vert_puzzle = puzzle.transpose
-  vert_puzzle.each do |element|
-    element.reverse
-  end
-  vert_puzzle[row_index]
-end
 
-def board_row(puzzle, row_index)
-  puzzle[row_index]
-end
 # [".87654321","2........","3........","4........","5........","6........","7........","8........","9........"]
 p is_valid_sudoku([".87654321","2........","3........","4........","5........","6........","7........","8........","9........"])
 # should be false
 p is_valid_sudoku(["....5..1.",".4.3.....",".....3..1","8......2.","..2.7....",".15......",".....2...",".2.9.....","..4......"])
+#should be false
+p is_valid_sudoku(["..4...63.",".........","5......9.","...56....","4.3.....1","...7.....","...5.....",".........","........."])
+
+require 'benchmark'
+
+n = 500
+Benchmark.bm(7) do |x|
+  x.report("for:")   { for i in 1..n; is_valid_sudoku([".87654321","2........","3........","4........","5........","6........","7........","8........","9........"]); end }
+  x.report("times:") { n.times do   ; is_valid_sudoku([".87654321","2........","3........","4........","5........","6........","7........","8........","9........"]); end }
+  x.report("upto:")  { 1.upto(n) do ; is_valid_sudoku([".87654321","2........","3........","4........","5........","6........","7........","8........","9........"]); end }
+end
+
+test_arr = ['.','.','4','.','.','.','6','3','.']
+
+p 'contains_duplicate'
+n = 50000
+Benchmark.bm(7) do |x|
+  x.report("for:")   { for i in 1..n; contains_duplicate(test_arr); end }
+  x.report("times:") { n.times do   ; contains_duplicate(test_arr); end }
+  x.report("upto:")  { 1.upto(n) do ; contains_duplicate(test_arr); end }
+end
+
+
+board = [".87654321","2........","3........","4........","5........","6........","7........","8........","9........"].map{|row| row.split('')}
+
+p 'add arr'
+n = 50000
+Benchmark.bm(7) do |x|
+  x.report("for:")   { for i in 1..n; board[0][0..2] + board[0 + 1][0..2] + board[0 + 2][0..2]; end }
+  x.report("times:") { n.times do   ; board[0][0..2] + board[0 + 1][0..2] + board[0 + 2][0..2]; end }
+  x.report("upto:")  { 1.upto(n) do ; board[0][0..2] + board[0 + 1][0..2] + board[0 + 2][0..2]; end }
+end
+
